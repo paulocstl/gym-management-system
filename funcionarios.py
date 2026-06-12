df3460461bc57f564d56ee17bffeb5301b63a941
paulocstln <paulocastelhano84@gmail.com>
2026-06-12 08:12:20 -0300

    atualizacao de erro do git

--- funcionarios.py
+++ funcionarios.py
@@ -1,7 +1,9 @@
+'''sistema para cadastro de funcionário'''
+
 funcionarios = []
 
 def cadastro_funcionarios():
-    print('===Cadastro de Funcionário===')
+    print('| Cadastro de Funcionário |')
     nome = input('Nome: ')
     cpf = input('CPF: ')
     cargo = input('Cargo: ')
@@ -24,12 +26,27 @@ def listar_funcionarios():
     for funcionario in funcionarios:
         print(f'Nome: {funcionario["nome"]}')
 
+# Terminar função de editar funcionário 
+
+def editar_funcionario():
+    print('| Editar Funcionário |')
+    cpf = input('Digite o CPF do funcionário a ser editado: ')
+    for funcionario in funcionarios:
+        if funcionario['cpf'] == cpf:
+            print('Funcionario encontrado. Digite os novos dados ou deixe em branco para manter o dado')
+            nome = input('Digite o novo nome: ')
+            cargo = input('Digite o novo cargo:')
+            salario = input('Digite o novo salário: ')
+            telefone = input('Digite o novo telefone: ')
+            horario = input('Digite o novo horário de trabalho: ')
+
+#Fazer teste para ver funcionamento 
 
 def menu_funcionarios():
 
     while True:
 
-        print("\n=== FUNCIONÁRIOS ===")
+        print("| FUNCIONÁRIOS |")
         print("1 - Cadastrar funcionário")
         print("2 - Listar funcionários")
         print('3 - Editar funcionário')
@@ -42,3 +59,7 @@ def menu_funcionarios():
             break
         elif opcao == '1':
             cadastro_funcionarios()
+
+#perguntar se tem algo a mudar para o professor             
+
+print('teste')
