0e18b9072ee054f0ffaac9f5cb54d2b557b80881
Inacio Mota <89469731+inslmotaa@users.noreply.github.com>
2026-06-17 22:05:37 -0300

    Update planos.py

--- planos.py
+++ planos.py
@@ -1,4 +1,20 @@
-planos = []
+import sqlite3
+
+conexao = sqlite3.connect("academia.db")
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS planos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    valor REAL,
+    duracao TEXT
+)
+""")
+
+conexao.commit()
+
+
 def menu_planos():
 
     while True:
@@ -22,32 +38,47 @@ def menu_planos():
         else:
             print("Opção inválida!")
 
+
 def cadastrar_plano():
+
     print("\n=== Cadastro de Plano ===")
 
     nome = input("Nome do plano: ")
+
     valor = float(input("Valor do plano (R$): "))
+
     duracao = input("Duração (Mensal, Trimestral, Semestral, Anual): ")
 
-    plano = {
-        "nome": nome,
-        "valor": valor,
-        "duracao": duracao
-    }
+    cursor.execute(
+        """
+        INSERT INTO planos (nome, valor, duracao)
+        VALUES (?, ?, ?)
+        """,
+        (nome, valor, duracao)
+    )
 
-    planos.append(plano)
+    conexao.commit()
 
     print("\nPlano cadastrado com sucesso!")
 
+
 def listar_planos():
+
     print("\n=== PLANOS CADASTRADOS ===")
+
+    cursor.execute("SELECT * FROM planos")
+
+    planos = cursor.fetchall()
 
     if len(planos) == 0:
         print("Nenhum plano cadastrado.")
         return
 
-    for i, plano in enumerate(planos, start=1):
-        print(f"{i}. {plano['nome']} - R${plano['valor']:.2f} - {plano['duracao']}")
+    for plano in planos:
 
-
-menu_planos()
+        print(
+            f"{plano[0]}. "
+            f"{plano[1]} - "
+            f"R${plano[2]:.2f} - "
+            f"{plano[3]}"
+        )
